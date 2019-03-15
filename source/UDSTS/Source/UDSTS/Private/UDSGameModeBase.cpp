1f754e3c2e3a04b81dffa1a90513f40a72f16172
bannified <freedomzelda_dc@hotmail.com>
2019-03-15 18:51:04 +0800

    Add BP variables to cpp UDSGameModeBase

--- source/UDSTS/Source/UDSTS/Private/UDSGameModeBase.cpp
+++ source/UDSTS/Source/UDSTS/Private/UDSGameModeBase.cpp
@@ -4,6 +4,21 @@
 #include "SpawnPoint.h"
 #include "Kismet/GameplayStatics.h"
 
+void AUDSGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	SetupSpawnPointGroups();
+
+}
+
+void AUDSGameModeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+
+}
+
 void AUDSGameModeBase::SetupSpawnPointGroups()
 {
 	TArray<AActor*> spawnPoints;
@@ -15,12 +30,20 @@ void AUDSGameModeBase::SetupSpawnPointGroups()
 		return;
 	}
 
+	SpawnPointsGroups.Add(FSpawnPointArray());
+
 	for (auto s : spawnPoints)
 	{
 		ASpawnPoint* spawnPoint = Cast<ASpawnPoint>(s);
 
+		SpawnPointsGroups[0].arr.Add(spawnPoint);
+
 		for (int number : spawnPoint->GroupNumbers)
 		{
+			if (number == 0)
+			{
+				continue;
+			}
 			// Resize SpawnPointGroups
 			if (number >= (SpawnPointsGroups.Num()))
 			{
--- source/UDSTS/Source/UDSTS/Public/UDSGameModeBase.h
+++ source/UDSTS/Source/UDSTS/Public/UDSGameModeBase.h
@@ -4,10 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "SpawnPointArray.h"
+#include "SpawnUnit.h"
+#include "WaveLayout.h"
 #include "GameFramework/GameModeBase.h"
 #include "UDSGameModeBase.generated.h"
 
-//struct FWaveLayout;
+class ULevelDataAsset;
 
 /**
  * 
@@ -18,12 +20,43 @@ class UDSTS_API AUDSGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Spawning")
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Level")
 	TArray< FSpawnPointArray > SpawnPointsGroups;
 
-private:
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level")
+	bool b_isWaveOngoing;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level")
+	int WaveNumber;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Level")
+	float WaveRunningTime;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Level")
+	ULevelDataAsset* MapLevelDataAsset;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Level")
+	FWaveLayout CurrentWave;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Level")
+	int CurrentSpawnUnitIndex;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Level")
+	FSpawnUnit NextSpawnUnit;
+
 	UFUNCTION(BlueprintCallable, Category = "Setup")
 	void SetupSpawnPointGroups();
+
+	UFUNCTION(BlueprintCallable, Category = "Spawn")
+	void SpawnWithSpawnUnit(const FSpawnUnit spawnUnit);
 
 };
