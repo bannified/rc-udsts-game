c7d4ea768a0ef3a9e738992167a595a7fd9048b3
bannified <freedomzelda_dc@hotmail.com>
2019-03-18 11:11:49 +0800

    Change GameModeState from an interface to subclass of AInfo (Actor)

--- source/UDSTS/Source/UDSTS/Public/GameModeState.h
+++ source/UDSTS/Source/UDSTS/Public/GameModeState.h
@@ -3,57 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Interface.h"
 #include "GameModeState.generated.h"
 
 class AUDSGameModeBase;
 
-// This class does not need to be modified.
-UINTERFACE(MinimalAPI)
-class UGameModeState : public UInterface
-{
-	GENERATED_BODY()
-};
-
-/**
- * 
- */
-class UDSTS_API IGameModeState
+UCLASS(config = Game, notplaceable, BlueprintType, Blueprintable, Transient, hideCategories = (Info, Rendering, MovementReplication, Replication, Actor), meta = (ShortTooltip = "Game Mode State defines the logic being carried out by the Game Mode (as a State Machine)."))
+class UDSTS_API AGameModeState : public AInfo
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	/**
 	 * Called when the GameMode enters this state.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Game Mode State")
-	void OnStateEnter(AUDSGameModeBase* GameMode);
+		void OnStateEnter(AUDSGameModeBase* GameMode);
 
 	/**
-	 * Called whenever this states starts executing. 
+	 * Called whenever this states starts executing.
 	 * Therefore, this method is called even when the GameMode is resumed (but still in this state).
 	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Game Mode State")
-	void OnStateStart(AUDSGameModeBase* GameMode);
+		void OnStateStart(AUDSGameModeBase* GameMode);
 
 	/**
 	 * Called every tick when this GameMode is active with this state.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Game Mode State")
-	void OnStateTick(AUDSGameModeBase* GameMode, float DeltaTime);
+		void OnStateTick(AUDSGameModeBase* GameMode, float DeltaTime);
 
 	/**
 	 * Called when state stops.
 	 * Therefore, this method is called even when the GameMode is paused (but still in this state).
 	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Game Mode State")
-	void OnStateStop(AUDSGameModeBase* GameMode);
+		void OnStateStop(AUDSGameModeBase* GameMode);
 
 	/**
 	 * Called when GameMode exits this state.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Game Mode State")
-	void OnStateExit(AUDSGameModeBase* GameMode);
-	
+		void OnStateExit(AUDSGameModeBase* GameMode);
+
 };
