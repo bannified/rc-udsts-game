294cab7c9db26e694422b158e244f3ed90f736d1
bannified <freedomzelda_dc@hotmail.com>
2019-07-29 00:29:50 +0800

    Implement BagBase's skeleton.

--- source/UDSTS/Source/UDSTS/Private/BagBase.cpp
+++ source/UDSTS/Source/UDSTS/Private/BagBase.cpp
@@ -2,13 +2,51 @@
 
 
 #include "BagBase.h"
+#include "UnrealNetwork.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/AudioComponent.h"
 
 // Sets default values
 ABagBase::ABagBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	SetRootComponent(SceneRoot);
 
+	BagMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BagMesh"));
+	BagMeshComponent->SetupAttachment(RootComponent);
+
+	BagMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BagMeshComponent->SetCollisionObjectType(ECC_WorldDynamic);
+	BagMeshComponent->SetCollisionResponseToAllChannels(ECR_Block);
+	BagMeshComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECR_Overlap);
+
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AudioComponent->SetupAttachment(BagMeshComponent);
+
+}
+
+bool ABagBase::IsMatterSufficient(float value)
+{
+	return value <= Matter;
+}
+
+void ABagBase::PlayBoostSound_Implementation()
+{
+
+}
+
+void ABagBase::ModifyMatterBy_Implementation(float deltaMatter, bool overflow /*= false*/)
+{
+
+}
+
+bool ABagBase::ModifyMatterBy_Validate(float deltaMatter, bool overflow /*= false*/)
+{
+	return true;
 }
 
 // Called when the game starts or when spawned
@@ -23,5 +61,11 @@ void ABagBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ABagBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME(ABagBase, Matter);
+	DOREPLIFETIME(ABagBase, MaxMatter);
 }
 
--- source/UDSTS/Source/UDSTS/Public/BagBase.h
+++ source/UDSTS/Source/UDSTS/Public/BagBase.h
@@ -6,6 +6,11 @@
 #include "GameFramework/Actor.h"
 #include "BagBase.generated.h"
 
+class USoundCue;
+class USceneComponent;
+class UStaticMeshComponent;
+class UAudioComponent;
+
 UCLASS()
 class UDSTS_API ABagBase : public AActor
 {
@@ -15,9 +20,40 @@ public:
 	// Sets default values for this actor's properties
 	ABagBase();
 
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	bool IsMatterSufficient(float value);
+
+	UFUNCTION(Server, Reliable, WithValidation, BlueprintCallable, Category = "Gameplay")
+	void ModifyMatterBy(float deltaMatter, bool overflow = false);
+
+	UFUNCTION(NetMulticast, Unreliable, BlueprintCallable, Category = "Gameplay")
+	void PlayBoostSound();
+
 protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* BagMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UAudioComponent* AudioComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TSubclassOf<UStaticMesh> BagMeshClassOverride;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadWrite)
+	float Matter;
+
+	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadWrite)
+	float MaxMatter;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TSubclassOf<USoundCue> SoundCueClass;
 
 public:	
 	// Called every frame
