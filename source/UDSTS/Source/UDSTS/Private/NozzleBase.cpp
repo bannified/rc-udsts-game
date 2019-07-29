f1e783d7638f234f640aff5eb3a93f243729e676
bannified <freedomzelda_dc@hotmail.com>
2019-07-30 00:08:40 +0800

    Add skeleton code for NozzleBase.

--- source/UDSTS/Source/UDSTS/Private/NozzleBase.cpp
+++ source/UDSTS/Source/UDSTS/Private/NozzleBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "NozzleBase.h"
+#include "UnrealNetwork.h"
 
 // Sets default values
 ANozzleBase::ANozzleBase()
@@ -9,19 +10,47 @@ ANozzleBase::ANozzleBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CurrentLevel = 1;
+	ObstacleTypes = { EObjectTypeQuery::ObjectTypeQuery1 };
+
+	SecondaryParticleScale = FVector(1.0f, 1.0f, 1.0f);
+	SecondaryParticleSystemSocketName = "ParticleSystemOut";
+
+	VelocityLimit = 200.0f;
+	DeadZoneLimit = 200.0f;
+}
+
+bool ANozzleBase::IsActorWithinRange(AActor* TargetActor)
+{
+	return true;
+}
+
+bool ANozzleBase::IsPathObstructed(AActor* TargetActor)
+{
+	return true;
 }
 
 // Called when the game starts or when spawned
 void ANozzleBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
 void ANozzleBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void ANozzleBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ANozzleBase, CurrentLevel);
+	DOREPLIFETIME(ANozzleBase, NozzleData);
+	DOREPLIFETIME(ANozzleBase, Character);
 
 }
 
--- source/UDSTS/Source/UDSTS/Public/NozzleBase.h
+++ source/UDSTS/Source/UDSTS/Public/NozzleBase.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Engine/EngineTypes.h"
 #include "NozzleBase.generated.h"
+
+class ACharacterBase;
+
+class UWeaponDataAsset;
+class UParticleSystem;
 
 UCLASS()
 class UDSTS_API ANozzleBase : public AActor
@@ -15,7 +21,41 @@ public:
 	// Sets default values for this actor's properties
 	ANozzleBase();
 
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	bool IsActorWithinRange(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	bool IsPathObstructed(AActor* TargetActor);
+
 protected:
+
+	/* Runtime Gameplay variables */
+	
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	int CurrentLevel;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	UWeaponDataAsset* NozzleData;
+	
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	ACharacterBase* Character;
+
+	/* Settings */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Gameplay")
+	TArray< TEnumAsByte<EObjectTypeQuery> > ObstacleTypes;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Gameplay")
+	TSubclassOf<UParticleSystem> SecondaryParticleSystem;
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Gameplay")
+	FVector SecondaryParticleScale;
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Gameplay")
+	FName SecondaryParticleSystemSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	float VelocityLimit;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	float DeadZoneLimit;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
